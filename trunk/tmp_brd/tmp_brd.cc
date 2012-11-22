86d2395ea074cff92ec86141d206e867ec6a1862
kevin.j.fox@gmail.com <kevin.j.fox@gmail.com@74955b97-cca5-4074-c841-0dd22d42026e>
2012-11-22 21:17:57 +0000

    Added tmp_brd example code.
    Note: Makefile configured for atmega168 (boarduino)....

---
+++ trunk/tmp_brd/tmp_brd.cc
@@ -0,0 +1,28 @@
+// read Analog port for 9V Power monitor
+
+
+#define StatusLed 13
+int powerInput = 1;
+int sensorValue = 0;
+int ledState = HIGH;
+
+void setup() {
+	// Define Serial Port
+	Serial.begin(57600);
+	Serial.println("Starting serial port...");
+	delay(2000);
+
+}
+
+
+void loop() {
+	// wait half a second
+	delay(500);
+	sensorValue = analogRead(powerInput);
+	Serial.print("PV = ");
+	Serial.println(sensorValue);
+	ledState = !ledState;
+	digitalWrite(StatusLed, ledState);
+	
+
+}
