1caaa2afc5221ab418331202d889fc469e6a1ee1
kevin.j.fox@gmail.com <kevin.j.fox@gmail.com@74955b97-cca5-4074-c841-0dd22d42026e>
2012-12-02 19:25:08 +0000

    Update to sprinkle project, includes current build tarball for sanguino (arduino-1.0.1 base)

--- trunk/boards/Sanguino/Sprinkle/Sprinkle.cc
+++ trunk/boards/Sanguino/Sprinkle/Sprinkle.cc
@@ -20,23 +20,41 @@
 //SoftwareSerial wifiSerial(14,13);
 // Initialize WiFly Software (Create instance...)
 WiFly wifly;
-int Pin_0 = 0;
-int Pin_1 = 1;
-int Pin_2 = 2;
-int Pin_3 = 3;
-int Pin_4 = 4;
 
+/*
+Map IO Port to Sprinkler Manifold Solnoids
+*/
+int Relay_0 = 5;
+int Relay_1 = 1;
+int Relay_2 = 2;
+int Relay_3 = 3;
+int Relay_4 = 4;
+int Relay_5 = 20;
+int Relay_6 = 19;
+int Relay_7 = 18;
+int Relay_8 = 17;
+int Relay_9 = 16;
+// Note Relays 0 & 15 not connected at this time.
 
 void terminal();
 
 void setup() {
 	char buf[32]; // 32 byte buffer
 
+	// Set Relay Pins to Outputs
+	pinMode(Relay_0, OUTPUT);
+	pinMode(Relay_1, OUTPUT);
+	pinMode(Relay_2, OUTPUT);
+	pinMode(Relay_3, OUTPUT);
+	pinMode(Relay_4, OUTPUT);
+	pinMode(Relay_5, OUTPUT);
+	pinMode(Relay_6, OUTPUT);
+	pinMode(Relay_7, OUTPUT);
+	pinMode(Relay_8, OUTPUT);
+	pinMode(Relay_9, OUTPUT);
+	
+	// Serial coniguration for devices
 	Serial.begin(57600); // serial to linux
-	pinMode(Pin_1, OUTPUT);
-	pinMode(Pin_2, OUTPUT);
-	pinMode(Pin_3, OUTPUT);
-	pinMode(Pin_4, OUTPUT);
 	Serial.println("Begin WiFi Config ");
 
 	delay(200); // wait for the wifi controller to start before initializing.
@@ -127,11 +145,11 @@ void setup() {
     Serial.print("Gateway: ");
     Serial.println(wifly.getGateway(buf, sizeof(buf)));
 
-    wifly.setDeviceID("Wifly-UDP");
-    Serial.print("DeviceID: ");
-    Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
+    //wifly.setDeviceID("Wifly-UDP");
+    //Serial.print("DeviceID: ");
+    //Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
 
-    wifly.setHost("10.42.8.35", 8042);	// Send UPD packets to this server and port
+    //wifly.setHost("10.42.8.35", 8042);	// Send UPD packets to this server and port
 
     Serial.println("WiFly ready");
 
@@ -139,29 +157,58 @@ void setup() {
 
 uint32_t lastSend = 0;
 uint32_t count=0;
-
+int delay_time = 500;
 void loop()
 {
-    digitalWrite(Pin_1, HIGH);
-    Serial.println("1 On");
-    delay(1500);
-    Serial.println("1 OFF");
-    digitalWrite(Pin_1, LOW);
-    digitalWrite(Pin_2, HIGH);
-    Serial.println("2 On");
-    delay(1500);
-    Serial.println("2 OFF");
-    digitalWrite(Pin_2, LOW);
-    digitalWrite(Pin_3, HIGH);
-    Serial.println("3 On");
-    delay(1500);
-    Serial.println("3 OFF");
-    digitalWrite(Pin_3, LOW);
-    digitalWrite(Pin_4, HIGH);
-    Serial.println("4 On");
-    delay(1500);
-    Serial.println("4 OFF");
-    digitalWrite(Pin_4, LOW);
+
+
+    digitalWrite(Relay_0, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_0, LOW);
+    
+    digitalWrite(Relay_1, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_1, LOW);
+    
+    digitalWrite(Relay_2, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_2, LOW);
+    
+    digitalWrite(Relay_3, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_3, LOW);
+    
+    digitalWrite(Relay_4, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_4, LOW);
+
+    digitalWrite(Relay_5, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_5, LOW);
+
+    digitalWrite(Relay_6, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_6, LOW);
+    
+    digitalWrite(Relay_7, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_7, LOW);
+    
+    digitalWrite(Relay_8, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_8, LOW);
+    
+    digitalWrite(Relay_9, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_9, LOW);
+
+
+    delay(delay_time);
+    delay(delay_time);
+
+
+
+
     /*
     if ((millis() - lastSend) > 1000) {
         count++;
--- trunk/boards/Sanguino/Sprinkle/applet/Sprinkle.cpp
+++ trunk/boards/Sanguino/Sprinkle/applet/Sprinkle.cpp
@@ -23,43 +23,41 @@ void loop();
 //SoftwareSerial wifiSerial(14,13);
 // Initialize WiFly Software (Create instance...)
 WiFly wifly;
-int Pin_0 = 0;
-int Pin_1 = 1;
-int Pin_2 = 2;
-int Pin_3 = 3;
-int Pin_4 = 4;
-int Pin_14 = 14;
-int Pin_20 = 20;
-int Pin_21 = 21;
-int Pin_17 = 17;
 
+/*
+Map IO Port to Sprinkler Manifold Solnoids
+*/
+int Relay_0 = 5;
+int Relay_1 = 1;
+int Relay_2 = 2;
+int Relay_3 = 3;
+int Relay_4 = 4;
+int Relay_5 = 20;
+int Relay_6 = 19;
+int Relay_7 = 18;
+int Relay_8 = 17;
+int Relay_9 = 16;
+// Note Relays 0 & 15 not connected at this time.
 
 void terminal();
 
 void setup() {
 	char buf[32]; // 32 byte buffer
 
+	// Set Relay Pins to Outputs
+	pinMode(Relay_0, OUTPUT);
+	pinMode(Relay_1, OUTPUT);
+	pinMode(Relay_2, OUTPUT);
+	pinMode(Relay_3, OUTPUT);
+	pinMode(Relay_4, OUTPUT);
+	pinMode(Relay_5, OUTPUT);
+	pinMode(Relay_6, OUTPUT);
+	pinMode(Relay_7, OUTPUT);
+	pinMode(Relay_8, OUTPUT);
+	pinMode(Relay_9, OUTPUT);
+	
+	// Serial coniguration for devices
 	Serial.begin(57600); // serial to linux
-	pinMode(Pin_1, OUTPUT);
-	pinMode(Pin_2, OUTPUT);
-	pinMode(Pin_3, OUTPUT);
-	pinMode(Pin_4, OUTPUT);
-	pinMode(Pin_14, OUTPUT);
-	pinMode(Pin_20, OUTPUT);
-	pinMode(Pin_21, OUTPUT);
-	pinMode(Pin_17, OUTPUT);
-	Serial.println("Powering On Pins 1-4");
-	digitalWrite(Pin_0, HIGH);
-	digitalWrite(Pin_1, HIGH);
-	digitalWrite(Pin_2, HIGH);
-	digitalWrite(Pin_3, HIGH);
-	digitalWrite(Pin_4, HIGH);
-	digitalWrite(Pin_14, HIGH);
-	digitalWrite(Pin_20, HIGH);
-	digitalWrite(Pin_21, HIGH);
-	digitalWrite(Pin_17, HIGH);
-
-/*
 	Serial.println("Begin WiFi Config ");
 
 	delay(200); // wait for the wifi controller to start before initializing.
@@ -150,44 +148,71 @@ void setup() {
     Serial.print("Gateway: ");
     Serial.println(wifly.getGateway(buf, sizeof(buf)));
 
-    wifly.setDeviceID("Wifly-UDP");
-    Serial.print("DeviceID: ");
-    Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
+    //wifly.setDeviceID("Wifly-UDP");
+    //Serial.print("DeviceID: ");
+    //Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
 
-    wifly.setHost("10.42.8.35", 8042);	// Send UPD packets to this server and port
+    //wifly.setHost("10.42.8.35", 8042);	// Send UPD packets to this server and port
 
     Serial.println("WiFly ready");
 
-*/
 }
 
 uint32_t lastSend = 0;
 uint32_t count=0;
-
+int delay_time = 500;
 void loop()
 {
-/*    
-    digitalWrite(Pin_1, HIGH);
-    Serial.println("1 On");
-    delay(1500);
-    Serial.println("1 OFF");
-    digitalWrite(Pin_1, LOW);
-    digitalWrite(Pin_2, HIGH);
-    Serial.println("2 On");
-    delay(1500);
-    Serial.println("2 OFF");
-    digitalWrite(Pin_2, LOW);
-    digitalWrite(Pin_3, HIGH);
-    Serial.println("3 On");
-    delay(1500);
-    Serial.println("3 OFF");
-    digitalWrite(Pin_3, LOW);
-    digitalWrite(Pin_4, HIGH);
-    Serial.println("4 On");
-    delay(1500);
-    Serial.println("4 OFF");
-    digitalWrite(Pin_4, LOW);
 
+
+    digitalWrite(Relay_0, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_0, LOW);
+    
+    digitalWrite(Relay_1, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_1, LOW);
+    
+    digitalWrite(Relay_2, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_2, LOW);
+    
+    digitalWrite(Relay_3, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_3, LOW);
+    
+    digitalWrite(Relay_4, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_4, LOW);
+
+    digitalWrite(Relay_5, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_5, LOW);
+
+    digitalWrite(Relay_6, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_6, LOW);
+    
+    digitalWrite(Relay_7, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_7, LOW);
+    
+    digitalWrite(Relay_8, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_8, LOW);
+    
+    digitalWrite(Relay_9, HIGH);
+    delay(delay_time);
+    digitalWrite(Relay_9, LOW);
+
+
+    delay(delay_time);
+    delay(delay_time);
+
+
+
+
+    /*
     if ((millis() - lastSend) > 1000) {
         count++;
 	Serial.print("Sending message ");
